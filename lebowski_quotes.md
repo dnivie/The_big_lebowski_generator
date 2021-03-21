145c34b0947b97eeeb3474db37f736a5fc271d3f
Eivind B Dammen <56489286+dnivie@users.noreply.github.com>
2021-03-21 22:40:17 +0100

    Add files via upload

---
+++ lebowski_quotes.md
@@ -0,0 +1,20 @@
+Obviously you're not a golfer.
+Hey, nice marmot!
+Fuck it, Dude. Let's go bowling.
+This is not 'Nam. This is bowling. There are rules!
+What’s a pederast, Walter?
+Do you have to use so many cuss words?
+I'm the Dude, so that's what you call me. That or, uh His Dudeness, or uh Duder, or El Duderino, if you’re not into the whole bevity hing.
+This aggression will not stand, man!
+This is what happens when you fuck a stranger in the ass, Larry!
+She's not my special lady, she's my fucking lady friend!
+Eight-year-olds, Dude.
+He fixes the cable.
+Careful man, there's a beverage here!
+Nobody fucks with the Jesus!
+Yeah, well, you know, that's just, like, your opinion, man!
+The Dude abides.
+Shut the fuck up, Donny!
+That rug really tied the room together.
+Um, I Am Not Mr. Lebowski. You're Mr. Lebowski. I'm The Dude.
+The Rug Pee-Ers Did Not Do This.
---
+++ random_sentence.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+import random
+import re
+from collections import defaultdict
+
+    
+
+def walk_graph(graph, distance=5, start_node=None):
+
+    if distance <= 0:
+        return []
+  
+  
+    if not start_node:
+        start_node = random.choice(list(graph.keys()))
+  
+  
+    weights = np.array(
+        list(markov_graph[start_node].values()),
+        dtype=np.float64)
+  
+    weights /= weights.sum()
+
+  
+    choices = list(markov_graph[start_node].keys())
+    chosen_word = np.random.choice(choices, None, p=weights)
+  
+    return [chosen_word] + walk_graph(
+        graph, distance=distance-1,
+        start_node=chosen_word)
+  
+
+
+if __name__ == "__main__":
+    
+    path = "lebowskiQuote.txt"
+    with open(path) as f:
+        text = f.read()
+    #print(re.split('\w+', text))
+    tokenized_text = [word for word in re.split('\W+', text) if word != '']
+
+    #print(tokenized_text)
+
+    markov_graph = defaultdict(lambda: defaultdict(int))
+
+    last_word = tokenized_text[0].lower()
+    for word in tokenized_text[1:]:
+        word = word.lower()
+        markov_graph[last_word][word] += 1
+        last_word = word
+
+        
+    for i in range(10):
+        print(' '.join(walk_graph(
+            markov_graph, distance=12)), '\n')
